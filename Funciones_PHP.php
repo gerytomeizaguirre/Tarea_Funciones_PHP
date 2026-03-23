7db235d2bbe3897b16e57daaf8f1dc50d136fa33
Gery Alem Tome Izaguirre <gery.tomeizaguirre@gmail.com>
2026-03-23 13:22:53 -0600

    Isset

--- Funciones_PHP.php
+++ Funciones_PHP.php
@@ -1,2 +1,12 @@
 <?php
+//isset();
+//isset: Determina si una variable está declarada y es distinta de null.
+//Devolverá como false al comprobar una variable a la que se le ha asignado el valor null.
+
+$nombre = "Gery";
+
+if (isset($nombre)) {
+    echo "Nombre Ingresado"; // Esto se imprime
+}
+
 
